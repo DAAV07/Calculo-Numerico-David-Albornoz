419dd638c7bd7343155329a3275860295d2b86ea
David_Albornoz <davidalejandroav16@gmail.com>
2002-01-01 06:41:56 -0400

    finished test biseccion

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ from biseccion import *
 
 class test(unittest.TestCase):
 	def test_biseccion_method(self):
-		self.assertEqual(biseccion("math.exp(-x) - math.log(x)",1,1.5,0.05,20),1.3125)
-		#self.assertEqual(biseccion("math.sin(x) + x - 1",0.57,1,0.001,20),0.5107177734374999)
+		f1 = lambda x: math.exp(-x) - math.log(x)
+		f2 = lambda x: pow((x-2),2) - math.log(x)
+		f3 = lambda x: math.sin(x) - math.exp(-x)
+		self.assertEqual(biseccion(f1,1,1.5,0.05,20),1.3125)
+		self.assertEqual(biseccion(f2,1,2,0.05,20),1.4375)
+		self.assertEqual(biseccion(f3,0,1,0.04,20),0.578125)
+	
 if __name__ == '__main__':
 	unittest.main()
